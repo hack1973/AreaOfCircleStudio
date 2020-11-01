c9b1d13eda61e64de99d83162f4ee4d2d580221e
Michael Hacker <hack1973@swbell.net>
2020-11-01 13:56:01 -0600

    cleaned up the code

--- AreaOfCircleStudio/Program.cs
+++ AreaOfCircleStudio/Program.cs
@@ -6,8 +6,6 @@ namespace AreaOfCircleStudio
     {
         static void Main(string[] args)
         {
-            //public const double PI = 3.1415926535897931;
-
             double radius;
             double mpg;
             double area;
@@ -28,16 +26,10 @@ namespace AreaOfCircleStudio
             diameter = 2 * radius;
             gallons = radius / mpg;
 
-            //double area1 = 3.14 * radius * radius;
             Console.WriteLine("\nThe area of a circle with a radius of " + radius + " is " + area);
             Console.WriteLine("The circumference of a circle with a radius of " + radius + " is " + circumference);
             Console.WriteLine("The diameter of a circle with a radius of " + radius + " is " + diameter);
             Console.WriteLine("You would use " + gallons + " gallons of gas if you drove the radius of " + radius + " miles getting " + mpg + " miles to the gallon.");
-
-            //Using the same radius, calculate the circumference (2*pi*r) and diameter of the circle (2*r)
-
-
-
         }
     }
 }
