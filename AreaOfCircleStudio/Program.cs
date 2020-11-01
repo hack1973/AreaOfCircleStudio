8715dcd938a9c3d31a06f28c84c8176e8716d771
Michael Hacker <hack1973@swbell.net>
2020-11-01 13:13:03 -0600

    init commit

--- AreaOfCircleStudio/Program.cs
+++ AreaOfCircleStudio/Program.cs
@@ -6,7 +6,7 @@ namespace AreaOfCircleStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
