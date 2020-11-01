ca73aa228cf94d7c022ef6d1c40960688a35bb72
Michael Hacker <hack1973@swbell.net>
2020-11-01 13:08:53 -0600

    Add project files.

---
+++ AreaOfCircleStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AreaOfCircleStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
